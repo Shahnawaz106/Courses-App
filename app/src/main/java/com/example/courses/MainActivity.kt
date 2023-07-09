0185d107484fc2ac42b3fb500a3a7285d01968d0
Shahnawaz Hussain <shahnawazh106@gmail.com>
2023-07-09 16:05:06 +0530

    Final Code

---
+++ app/src/main/java/com/example/courses/MainActivity.kt
@@ -0,0 +1,130 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
+package com.example.courses
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.courses.data.DataSource
+import com.example.courses.model.Topic
+import com.example.courses.ui.theme.CoursesTheme
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CoursesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TopicGrid(
+                        modifier = Modifier.padding(dimensionResource(R.dimen.padding_small))
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun TopicGrid(modifier: Modifier = Modifier) {
+
+    LazyVerticalGrid(
+        cells = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
+        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
+        modifier = modifier
+    ) {
+        items(DataSource.topics) { topic ->
+            TopicCard(topic)
+        }
+    }
+}
+@Composable
+fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
+        Card {
+            Row {
+                Box {
+                    Image(
+                        painter = painterResource(id = topic.imageRes),
+                        contentDescription = null,
+                        modifier = modifier
+                            .size(width = 68.dp, height = 68.dp)
+                            .aspectRatio(1f),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+
+                Column {
+                    Text(
+                        text = stringResource(id = topic.name),
+                        style = MaterialTheme.typography.body2,
+                        modifier = Modifier.padding(
+                            start = dimensionResource(R.dimen.padding_medium),
+                            top = dimensionResource(R.dimen.padding_medium),
+                            end = dimensionResource(R.dimen.padding_medium),
+                            bottom = dimensionResource(R.dimen.padding_small)
+                        )
+                    )
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_grain),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .padding(start = dimensionResource(R.dimen.padding_medium))
+                        )
+                        Text(
+                            text = topic.availableCourses.toString(),
+                            style = MaterialTheme.typography.body2,
+                            modifier = Modifier.padding(start = dimensionResource(R.dimen.padding_small))
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+@Preview(showBackground = true)
+@Composable
+fun TopicPreview() {
+    CoursesTheme {
+        val topic = Topic(R.string.photography, 321, R.drawable.photography)
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TopicCard(topic = topic)
+        }
+    }
+}
